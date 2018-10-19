c439c776a11fef878ae57c4f62858f2a1a029538
zengjiahao <zengjiahao@Jperation.com>
2018-10-19 19:19:05 +0800

    feat(wxapp): fix配置

--- src/plugins/index.js
+++ src/plugins/index.js
@@ -3,5 +3,5 @@ import Vue from 'vue'
 const pluginsContext = require.context('@/plugins', true, /^.\/\w+\/index\.js$/)
 
 pluginsContext.keys().forEach((plguin) => {
-  Vue.use(plguin)
+  Vue.use(pluginsContext(plguin).default)
 })
