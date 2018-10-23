e859301721ceeb70c6882b876fa35794d2822061
Jayden <848099959@qq.com>
2018-10-23 23:41:53 +0800

    油画规则最新

---
+++ src/components/rule.vue
@@ -0,0 +1,73 @@
+<template>
+  <ul class="rule-raido_sel">
+    <li v-for = "item in ruleData" :key = "item" :id = "item.id" @click = "openRule(item)" :class="selectId === item.id ? 'on' : ''">
+      <div class="rule-list_title">{{ item.title }}</div>
+      <div class="rule-list_content">
+        {{ item.content }}
+      </div>
+    </li>
+  </ul>
+</template>
+
+<script>
+  export default {
+    props: {
+      ruleData: Object,
+      selectId: null
+    },
+    mounted() {
+      
+    },
+    methods: {
+      openRule(item) {
+        this.selectId = item.id;
+      }
+    }
+  }
+</script>
+
+<style lang="postcss">
+  .rule-raido_sel {
+    margin-bottom: 20rpx;
+    padding: 0 40rpx;
+    background-color: #fff;
+    overflow: hidden;
+  }
+
+  .rule-raido_sel li {
+    height: 100rpx;
+    border-bottom: 1px solid #EEEEEE;
+  }
+
+  .rule-raido_sel :last-child {
+    border-bottom: none;
+  }
+
+  .rule-raido_sel .rule-list_title {
+    height: 100rpx;
+    line-height: 100rpx;
+    font-size: 32rpx;
+    color: #333;
+  }
+
+  .rule-raido_sel .rule-list_content {
+    padding-left: 40rpx;
+    margin-bottom: 20rpx;
+    font-size: 32rpx;
+    line-height: 2;
+    color: #333;
+    display: none;
+  }
+
+  .rule-raido_sel li.on {
+    height: auto;
+  }
+
+  .rule-raido_sel li.on .rule-list_title {
+    font-size: 36rpx;
+  }
+
+  .rule-raido_sel li.on .rule-list_content {
+    display: block;
+  }
+</style>
--- src/pages/commonRule/commonRule.vue
+++ src/pages/commonRule/commonRule.vue
@@ -1,11 +1,12 @@
 <template>
   <div class="rule">
     <div class="rule_title">共享油画规则</div>
+    <rule :ruleData = "ruleList"></rule>
     <div class="about_msg">
       <div class="msg_title">联系我们</div>
       <ul class="msg_content">
         <li class="msg_item" v-for = "item in msg" :key = "item">
-          <label for="">{{item.title}}</label><span>{{item.value}}</span>
+          <label for="">{{ item.title }}</label><span>{{ item.value }}</span>
         </li>
       </ul>
     </div>
@@ -13,7 +14,12 @@
 </template>
 
 <script>
+  import rule from '../../components/rule'
+
   export default {
+    components: {
+      rule
+    },
     data() {
       return {
         msg: [
@@ -21,7 +27,7 @@
             title: "拨打客服电话：",
             value: "200-253-4500"
           },{
-            title: "客服服务时间",
+            title: "客服服务时间：",
             value: "08：00~22：00"
           },{
             title: "在官方微信中留言：",
@@ -32,6 +38,21 @@
           },{
             title: "原装品牌合作：",
             value: "ghjhsdhj@123.net"
+          }
+        ],
+        ruleList: [
+          {
+            id: 0,
+            title: "Z.如何尊享租画体验",
+            content: "完成享受租画体验步骤如下：1.注册XXX会员。2.在页面选择自己喜欢的油画添加到购物车。3.确认下单（将这幅油画寄给我）。4.填写快递信息（如：姓名、电话、签收地址）。5.等待送达并查验商品签收。"
+          },{
+            id: 1,
+            title: "Z.如何尊享租画体验",
+            content: "完成享受租画体验步骤如下：1.注册XXX会员。2.在页面选择自己喜欢的油画添加到购物车。3.确认下单（将这幅油画寄给我）。4.填写快递信息（如：姓名、电话、签收地址）。5.等待送达并查验商品签收。"
+          },{
+            id: 2,
+            title: "Z.如何尊享租画体验",
+            content: "完成享受租画体验步骤如下：1.注册XXX会员。2.在页面选择自己喜欢的油画添加到购物车。3.确认下单（将这幅油画寄给我）。4.填写快递信息（如：姓名、电话、签收地址）。5.等待送达并查验商品签收。"
           }
         ]
       }
@@ -57,13 +78,13 @@
     line-height: 64rpx;
     font-size: 64rpx;
     color: #ccc;
-    padding: 40rpx 40rpx 60rpx;
+    padding: 40rpx;
     background-color: #fff;
   }
 
   .about_msg .msg_title {
     line-height: 64rpx;
-    font-size: 32rpx;
+    font-size: 24rpx;
     padding: 0 40rpx;
     margin-top: 20rpx;
     background-color: #fff;
@@ -78,7 +99,7 @@
   .about_msg .msg_content .msg_item {
     margin-bottom: 24rpx;
     line-height: 46rpx;
-    font-size: 28rpx;
+    font-size: 24rpx;
   }
 
   .about_msg .msg_content .msg_item > span {
