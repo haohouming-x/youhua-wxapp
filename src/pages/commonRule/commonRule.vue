4e98e5586ca141e01a4f7debb03d70593154478a
Jayden <848099959@qq.com>
2018-10-23 21:49:34 +0800

    feat():暂存

--- src/pages/commonRule/commonRule.vue
+++ src/pages/commonRule/commonRule.vue
@@ -1,6 +1,12 @@
 <template>
-  <div class="rule_title">
-    共享油画规则
+  <div class="rule">
+    <div class="rule_title">共享油画规则</div>
+    <div class="about_msg">
+      <div class="msg_title">联系我们</div>
+      <ul class="msg_content">
+        <li class="msg_item"></li>
+      </ul>
+    </div>
   </div>
 </template>
 
@@ -19,10 +25,27 @@
 </script>
 
 <style lang='postcss'>
+  page {
+    background-color: #f5f5f5;
+  }
+
  .rule_title {
     line-height: 64rpx;
     font-size: 64rpx;
     color: #ccc;
     padding: 40rpx 40rpx 60rpx;
+    background-color: #fff;
+  }
+
+  .about_msg .msg_title {
+    line-height: 64rpx;
+    font-size: 32rpx;
+    padding: 0 40rpx;
+    margin-top: 20rpx;
+    background-color: #fff;
+  }
+
+  .about_msg .msg_content .msg_item {
+    border-bottom: 1rpx solid #ddd;
   }
 </style>
